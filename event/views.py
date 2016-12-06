b3a2cc4d001820c17eda867fa4b7cc3eb1312603
Bing <bl1861@nyu.edu>
2016-12-05 22:17:50 -0500

    update

--- event/views.py
+++ event/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def event_all(request):
-	context = {'login': False}
-	if 'username' in request.session:
-		context['login'] = True
-	return render(request, 'event/event_all.html', context)
-
-
 def event(request, id):
 	context = {'login': False}
 	if 'username' in request.session:
