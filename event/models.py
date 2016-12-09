4c68ac7026aa7a1f8592d41ac94cf3467d820bd9
Bing <bl1861@nyu.edu>
2016-12-09 17:17:27 -0500

    update

--- event/models.py
+++ event/models.py
@@ -1,3 +1,57 @@
+from __future__ import unicode_literals
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    uname = models.CharField(primary_key=True, max_length=50)
+    login_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    udescription = models.CharField(max_length=200, blank=True, null=True)
+    ufile = models.FileField(blank=True, null=True)
+    uphoto = models.FileField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = '_user'
+
+class Egroup(models.Model):
+    gid = models.AutoField(primary_key=True)
+    gname = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'egroup'
+        
+
+class GroupUser(models.Model):
+    uname = models.ForeignKey(User, models.DO_NOTHING, db_column='uname')
+    gid = models.ForeignKey(Egroup, models.DO_NOTHING, db_column='gid')
+
+    class Meta:
+        managed = False
+        db_table = 'group_user'
+        unique_together = (('uname', 'gid'),)
+
+
+
+class Event(models.Model):
+    eid = models.AutoField(primary_key=True)
+    ename = models.CharField(max_length=30, blank=True, null=True)
+    etime = models.DateTimeField(blank=True, null=True)
+    gid = models.ForeignKey(Egroup, models.DO_NOTHING, db_column='gid', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'event'
+
+
+class Rsvp(models.Model):
+    eid = models.ForeignKey(Event, models.DO_NOTHING, db_column='eid')
+    uname = models.ForeignKey(User, models.DO_NOTHING, db_column='uname')
+
+    class Meta:
+        managed = False
+        db_table = 'rsvp'
+        unique_together = (('uname', 'eid'),)
--- group/urls.py
+++ group/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.group_all, name='group_all'),
-	url(r'^(?P<id>\d+)/$', views.group, name='group'),
+	url(r'^(?P<id>\d+)/$', views.join_group, name='join_group'),
 ]
